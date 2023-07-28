57bbd7b5e248e5d21b2b583f636983601051b0a5
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-07-28 13:37:52 +0630

    tile Map

--- src/TileMap.ts
+++ src/TileMap.ts
@@ -2,7 +2,6 @@ import Container from "./Container";
 import Texture from "./Texture";
 import TileSprite from "./TileSprite";
 import { Frame } from "./interfaces";
-
 export default class TileMap extends Container<TileSprite> {
   mapW: number;
   mapH: number;
@@ -21,7 +20,11 @@ export default class TileMap extends Container<TileSprite> {
 
     for(let y = 0; y < mapH; y++) {
         for(let x = 0; x < mapW; x++) {
-            const tile = new TileSprite(texture, tileSize, tileSize, { x: 0, y: 0});
+            const index = y * mapW + x;
+            const tile = new TileSprite(texture, tileSize, tileSize, frames[index]);
+            tile.pos.x = x * tileSize;
+            tile.pos.y = y * tileSize;
+            this.children.push(tile);
         }
     }
   }
