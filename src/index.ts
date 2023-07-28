942df2a5fa42ad4ac40c99664a2e39cf0f8bc070
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-07-28 12:49:57 +0630

    tileMap

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import "./style.css";
 import Container from "./Container";
 import DebugGrid from "./DebugGrid";
 import Game from "./Game";
-import Sprite from "./Sprite";
 import Texture from "./Texture";
-import TileSprite from "./TileSprite";
 const cellSize = 64;
 let w = cellSize * 9;
 let h = cellSize * 6;
@@ -13,15 +11,10 @@ let { scene } = game;
 
 const container = new Container();
 const texture = new Texture(
-  "https://raw.githubusercontent.com/yanmoenaing118/canvas/main/public/spider10.png"
+  "https://raw.githubusercontent.com/yanmoenaing118/canvas/main/public/dungeon.png"
 );
-const tileSprite = new TileSprite(texture, cellSize, cellSize, { x: 0, y: 0 });
-const sprite = new Sprite(texture);
 
-container.add(tileSprite);
 scene.add(container);
-scene.add(tileSprite);
-scene.add(sprite);
 
 function update(dt: number, t: number) {
   console.log(dt, t);
