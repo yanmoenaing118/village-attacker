c4971508152a2970077b558fb204af8f3820914e
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-08 19:54:52 +0630

    game over screen

---
+++ src/screens/GameOverScreen.ts
@@ -0,0 +1,10 @@
+import Container from "../Container";
+import Entity from "../Entity";
+
+export default class GameOverScreen extends Container<Entity> {
+
+    constructor() {
+        super();
+    }
+
+}
