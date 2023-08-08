b9601d234ec2ccfe11ff4c73776c4e91855fae37
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-08 19:56:24 +0630

    game over

--- src/screens/GameOverScreen.ts
+++ src/screens/GameOverScreen.ts
@@ -1,10 +1,16 @@
 import Container from "../Container";
 import Entity from "../Entity";
 
+/**
+ * Game Over Text
+ * Restart Button
+ * Home Screen
+ *
+ * clicking restart -> restart the game
+ * clicking home -> startScreen
+ */
 export default class GameOverScreen extends Container<Entity> {
-
-    constructor() {
-        super();
-    }
-
+  constructor() {
+    super();
+  }
 }
